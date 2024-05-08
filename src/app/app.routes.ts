8d9d2f5261de29ee54f97ec974d441c8f0c72943
María Cortés <cortes13zgz@gmail.com>
2024-05-08 20:58:44 +0200

    app routes and app config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,22 @@ import { Routes } from '@angular/router';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
 
 export const routes: Routes = [
-    
+    {
+        path: 'list',
+        children: [
+            {
+                path: ':idMovie',
+                loadComponent: () => import('./components/movie-detail/movie-detail.component').then(mov => mov.MovieDetailComponent)
+            },
+            {
+                path: '',
+                component: MovieListComponent
+            }
+        ]
+    },
+    {
+        path:'**',
+        redirectTo: '/list',
+        pathMatch: 'full'
+    }
 ];
